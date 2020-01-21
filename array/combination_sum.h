bf96093f4702d480049f11a27ed51d52141ac49f
qingya <qingyachu@foxmail.com>
2020-01-22 00:14:09 +0800

    [feature] Combination of no duplication.

--- array/combination_sum.h
+++ array/combination_sum.h
@@ -29,6 +29,27 @@ namespace combination_sum {
             return ans_;
         }
 
+        void RecursiveCombinationSum2(int start, int target) {
+            if (target == 0) {
+                ans_.push_back(path_);
+                return;
+            }
+
+            for (auto i = start; i < input_.size() && target - input_[i] >= 0; ++i) {
+                path_.push_back(input_[i]);
+                RecursiveCombinationSum2(i + 1, target - input_[i]);
+                path_.pop_back();
+                while (i + 1 < input_.size() && input_[i] == input_[i + 1]) ++i;
+            }
+        }
+
+        vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+            input_ = candidates;
+            std::sort(input_.begin(), input_.end());
+            RecursiveCombinationSum2(0, target);
+            return ans_;
+        }
+
     private:
         std::vector<int> path_;
         std::vector<std::vector<int>> ans_;
@@ -37,8 +58,8 @@ namespace combination_sum {
 
     void Test() {
         Solution so;
-        std::vector<int> input{ 2,3,6,7 };
-        auto ans = so.combinationSum(input, 7);
+        std::vector<int> input{ 10,1,2,7,6,1,5 };
+        auto ans = so.combinationSum2(input, 1);
         for (int i = 0; i < ans.size(); ++i) {
             for (int j = 0; j < ans[i].size(); ++j) {
                 std::cout << ans[i][j] << " ";
